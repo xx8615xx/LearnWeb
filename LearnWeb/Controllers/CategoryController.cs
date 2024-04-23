c97d67a66591e88c7d3b93c03001ecba40de1d1c
Pika <xx8615xx@gmail.com>
2024-04-23 13:11:16 +0800

    Section2: a category page for basic CRUD.

---
+++ LearnWeb/Controllers/CategoryController.cs
@@ -0,0 +1,95 @@
+﻿using LearnWeb.Data;
+using LearnWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db){
+            _db = db; 
+        }
+        public IActionResult Index()
+        {
+            List<Category> categoryList = _db.Categories.ToList();
+            return View(categoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name","Category Name shold not be the same with Display Order");
+            //}
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully.";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0){
+                return NotFound();
+            }
+            Category? categoryFromDB = _db.Categories.Find(id);
+            //Category? categoryFromDB2 = _db.Categories.FirstOrDefault(u=>u.ID==id);
+            //Category? categoryFromDB3 = _db.Categories.Where(u=>u.ID==id).FirstOrDefault();
+            if (categoryFromDB == null){
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category edited successfully.";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDB = _db.Categories.Find(id);
+            //Category? categoryFromDB2 = _db.Categories.FirstOrDefault(u=>u.ID==id);
+            //Category? categoryFromDB3 = _db.Categories.Where(u=>u.ID==id).FirstOrDefault();
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if(obj == null){
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully.";
+            return RedirectToAction("Index", "Category");
+        }
+    }
+}
