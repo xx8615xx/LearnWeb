db25e5227f95400de4a58f5c530d8d07dc78b452
Pika <xx8615xx@gmail.com>
2024-04-26 21:07:45 +0800

    add role and email container

--- Learn.Utility/SD.cs
+++ Learn.Utility/SD.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Learn.Utility
 {
-    internal class SD
+    public class SD
     {
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
     }
 }
--- LearnWeb/Program.cs
+++ LearnWeb/Program.cs
@@ -3,6 +3,8 @@ using Learn.DataAccess.Repository;
 using Learn.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Learn.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +14,10 @@ builder.Services.AddDbContext<AppDbContext>(options=>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddRazorPages();
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
+//builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
