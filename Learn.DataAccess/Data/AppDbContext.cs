6707e00dcc1d9ae98eb227f314be43232059399a
Pika <xx8615xx@gmail.com>
2024-04-26 17:14:11 +0800

    Add identity nuget

--- Learn.DataAccess/Data/AppDbContext.cs
+++ Learn.DataAccess/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Learn.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Learn.DataAccess.Data
 {
-    public class AppDbContext: DbContext
+    public class AppDbContext: IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
@@ -15,6 +16,8 @@ namespace Learn.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { ID = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { ID = 2, Name = "SciFi", DisplayOrder = 2 },
