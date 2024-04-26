d4fb1c2537cc41da989b81219ca682a33fffb75e
Pika <xx8615xx@gmail.com>
2024-04-26 19:20:30 +0800

    Debug: Identity page is razorpage and update-database

--- LearnWeb/Program.cs
+++ LearnWeb/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options=> 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddRazorPages();
 builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
@@ -35,5 +36,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
