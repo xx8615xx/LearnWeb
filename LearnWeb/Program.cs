3316907ef7cd3dd2a11a5da3a3e1e597ca3cc5be
Pika <xx8615xx@gmail.com>
2024-04-26 17:18:57 +0800

    Add scaffold identity

--- LearnWeb/Program.cs
+++ LearnWeb/Program.cs
@@ -2,6 +2,7 @@ using Learn.DataAccess.Data;
 using Learn.DataAccess.Repository;
 using Learn.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options=> 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
