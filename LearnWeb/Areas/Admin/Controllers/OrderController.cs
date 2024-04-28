07dcf167dac2a019bbe9156a14c311b222c8bd09
Pika <xx8615xx@gmail.com>
2024-04-28 22:07:17 +0800

    update details

--- LearnWeb/Areas/Admin/Controllers/OrderController.cs
+++ LearnWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,7 +1,10 @@
 ﻿using Learn.DataAccess.Repository.IRepository;
 using Learn.Models;
+using Learn.Models.ViewModels;
 using Learn.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe.Climate;
 using System.Diagnostics;
 
 namespace LearnWeb.Areas.Admin.Controllers
@@ -10,7 +13,10 @@ namespace LearnWeb.Areas.Admin.Controllers
 	public class OrderController : Controller
 	{
 		private readonly IUnitOfWork _unitOfWork;
-		public OrderController(IUnitOfWork unitOfWork) {
+
+        [BindProperty]
+        public OrderVM OrderVM { get; set; }
+        public OrderController(IUnitOfWork unitOfWork) {
 			_unitOfWork = unitOfWork;
 		}
 		public IActionResult Index()
@@ -18,8 +24,45 @@ namespace LearnWeb.Areas.Admin.Controllers
 			return View();
 		}
 
-		#region API CALLS
-		[HttpGet]
+        public IActionResult Details(int orderID)
+        {
+            OrderVM = new()
+            {
+                OrderHeader=_unitOfWork.OrderHeader.Get(u=>u.ID==orderID,includeProperties:"ApplicationUser"),
+                OrderDetail=_unitOfWork.OrderDetail.GetAll(u=>u.OrderHeaderID==orderID,includeProperties:"Product")
+            };
+
+            return View(OrderVM);
+        }
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin+","+SD.Role_Employee)]
+        public IActionResult UpdateOrderDetail()
+        {
+            var orderHeaderFromDb = _unitOfWork.OrderHeader.Get(u => u.ID == OrderVM.OrderHeader.ID);
+            orderHeaderFromDb.Name = OrderVM.OrderHeader.Name;
+            orderHeaderFromDb.PhoneNumber = OrderVM.OrderHeader.PhoneNumber;
+            orderHeaderFromDb.StreetAddress = OrderVM.OrderHeader.StreetAddress;
+            orderHeaderFromDb.City = OrderVM.OrderHeader.City;
+            orderHeaderFromDb.State = OrderVM.OrderHeader.State;
+            orderHeaderFromDb.PostalCode = OrderVM.OrderHeader.PostalCode;
+            if (!string.IsNullOrEmpty(OrderVM.OrderHeader.Carrier))
+            {
+                orderHeaderFromDb.Carrier = OrderVM.OrderHeader.Carrier;
+            }
+            if (!string.IsNullOrEmpty(OrderVM.OrderHeader.TrackingNumber))
+            {
+                orderHeaderFromDb.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+            }
+            _unitOfWork.OrderHeader.Update(orderHeaderFromDb);
+            _unitOfWork.Save();
+
+            TempData["success"] = "Order Details Updated Successfully.";
+
+            return RedirectToAction(nameof(Details), new { orderID = orderHeaderFromDb.ID});
+        }
+
+        #region API CALLS
+        [HttpGet]
 		public IActionResult GetAll(string status)
 		{
 			IEnumerable<OrderHeader> data = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
