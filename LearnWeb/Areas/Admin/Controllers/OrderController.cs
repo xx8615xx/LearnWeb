3f8d1fa53339cd924d2b7d1692a61a1d1d38c127
Pika <xx8615xx@gmail.com>
2024-04-28 19:48:19 +0800

    order filter

--- LearnWeb/Areas/Admin/Controllers/OrderController.cs
+++ LearnWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,8 @@
 ﻿using Learn.DataAccess.Repository.IRepository;
 using Learn.Models;
+using Learn.Utility;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace LearnWeb.Areas.Admin.Controllers
 {
@@ -18,10 +20,28 @@ namespace LearnWeb.Areas.Admin.Controllers
 
 		#region API CALLS
 		[HttpGet]
-		public IActionResult GetAll()
+		public IActionResult GetAll(string status)
 		{
-			List<OrderHeader> data = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
-			return Json(new { data });
+			IEnumerable<OrderHeader> data = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            switch (status)
+            {
+                case "pending":
+					data = data.Where(u=>u.PaymentStatus==SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    data = data.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    data = data.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    data = data.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data });
 
 		}
 		#endregion
--- LearnWeb/wwwroot/js/order.js
+++ LearnWeb/wwwroot/js/order.js
@@ -1,16 +1,31 @@
 ﻿var dataTable;
 $(document).ready(function () {
-    loadDataTable();
+    var url = window.location.search;
+    if (url.includes("pending")) {
+        loadDataTable("pending");
+    }
+    else if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else if (url.includes("completed")) {
+        loadDataTable("completed");
+    }
+    else if (url.includes("approved")) {
+        loadDataTable("approved");
+    }
+    else {
+        loadDataTable("all");
+    }
 });
 
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url: '/admin/order/getall'},
+        "ajax": { url: '/admin/order/getall?status='+ status},
         "columns": [
             { data: 'id', "width":"5%" },
-            { data: 'name', "width": "15%" },
+            { data: 'name', "width": "25%" },
             { data: 'phoneNumber', "width": "20%" },
-            { data: 'applicationUser.email', "width": "15%" },
+            { data: 'applicationUser.email', "width": "20%" },
             { data: 'orderStatus', "width": "10%" },
             { data: 'orderTotal', "width": "10%" },
             {
@@ -21,7 +36,7 @@ function loadDataTable() {
                      <a href="/admin/order/details?orderID=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-fill"></i></a>
                      </div>`
                 },
-                width:"25%"
+                width:"10%"
             }
         ]
     });
