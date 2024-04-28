571dbbc6b63c6bf724b98e249d27cc32862db5c4
Pika <xx8615xx@gmail.com>
2024-04-28 18:29:23 +0800

    add order page

---
+++ LearnWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using Learn.DataAccess.Repository.IRepository;
+using Learn.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnWeb.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public OrderController(IUnitOfWork unitOfWork) {
+			_unitOfWork = unitOfWork;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> data = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			return Json(new { data });
+
+		}
+		#endregion
+	}
+}
